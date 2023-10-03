26f0d9d1673bd8e7d189a71c5761ac805d45d5c7
Quantumapple <jongapples@gmail.com>
2023-10-03 07:56:11 -0500

    simply init, add more functions

--- TestBeam/beamtest_analysis_helper.py
+++ TestBeam/beamtest_analysis_helper.py
@@ -17,13 +17,8 @@ class etroc2_analysis_helper():
 
     def __init__(
             self,
-            chip_names: list,
-            chip_figtitles: list,
-            chip_labels: list,
         ):
-        self.chip_names = chip_names,
-        self.chip_figtitles = chip_figtitles,
-        self.chip_labels = chip_labels,
+        pass
 
     ## --------------------------------------
     def toSingleDataFrame(
@@ -80,22 +75,22 @@ class etroc2_analysis_helper():
             pass
 
         return df
+
     ## --------------------------------------
     def toSingleDataFramePerDirectory(
             self,
+            root: str,
             path_pattern: str,
-            files: list,
         ):
 
         evt = -1
         previous_bcid = -1
         df_count = 0
-
-        root = '../../ETROC-Data'
         name_pattern = "*translated*.dat"
 
         dirs = glob(f"{root}/{path_pattern}")
         dirs = natsorted(dirs)
+        print(dirs)
 
         for dir in dirs:
             d = []
@@ -136,8 +131,6 @@ class etroc2_analysis_helper():
             df = pd.DataFrame(d)
             df.to_parquet(name+'.pqt', index=False)
             del df
-    ## --------------------------------------
-
 
     ## --------------------------------------
     def making_heatmap_byPandas(
@@ -162,7 +155,7 @@ class etroc2_analysis_helper():
                 fill_value=0  # Fill missing values with 0 (if any)
             )
 
-            if pivot_table.shape[1] != 16:
+            if (pivot_table.shape[0] != 16) or (pivot_table.shape[1]!= 16):
                 pivot_table = pivot_table.reindex(pd.Index(np.arange(0,16), name='')).reset_index()
                 pivot_table = pivot_table.reindex(columns=np.arange(0,16))
                 pivot_table = pivot_table.fillna(-1)
@@ -197,6 +190,155 @@ class etroc2_analysis_helper():
             ax.invert_yaxis()
             plt.minorticks_off()
 
+    ## --------------------------------------
+    def singlehit_event_clear_func(
+            self,
+            input_df: pd.DataFrame
+        ):
+        # Group the DataFrame by 'evt' and count unique 'board' values in each group
+        unique_board_counts = input_df.groupby('evt')['board'].nunique()
+
+        ## event has two unique board ID
+        event_numbers_with_three_unique_boards = unique_board_counts[unique_board_counts == 3].index
+        subset_df = input_df[input_df['evt'].isin(event_numbers_with_three_unique_boards)]
+        subset_df.reset_index(inplace=True, drop=True)
+
+        ## event has one hit from each board
+        event_board_counts = subset_df.groupby(['evt', 'board']).size().unstack(fill_value=0)
+        selected_event_numbers = event_board_counts[(event_board_counts[0] == 1) & (event_board_counts[1] == 1) & (event_board_counts[3] == 1)].index
+        selected_subset_df = subset_df[subset_df['evt'].isin(selected_event_numbers)]
+        selected_subset_df.reset_index(inplace=True, drop=True)
+        del subset_df
+
+        return selected_subset_df
+
+    ## --------------------------------------
+    def tdc_event_selection(
+            self,
+            input_df: pd.DataFrame,
+            tdc_cuts_dict: dict
+        ):
+        # Create boolean masks for each board's filtering criteria
+        masks = {}
+        for board, cuts in tdc_cuts_dict.items():
+            mask = (
+                (input_df['board'] == board) &
+                input_df['cal'].between(cuts[0], cuts[1]) &
+                input_df['toa'].between(cuts[2], cuts[3]) &
+                input_df['tot'].between(cuts[4], cuts[5])
+            )
+            masks[board] = mask
+
+        # Combine the masks using logical OR
+        combined_mask = pd.concat(masks, axis=1).any(axis=1)
+
+        # Apply the combined mask to the DataFrame
+        tdc_filtered_df = input_df[combined_mask].reset_index(drop=True)
+
+        return tdc_filtered_df
+
+    ## --------------------------------------
+    def pixel_filter(
+            self,
+            input_df: pd.DataFrame,
+            pixel_dict: dict
+        ):
+        # Create boolean masks for each board's filtering criteria
+        masks = {}
+        for board, pix in pixel_dict.items():
+            mask = (
+                (input_df['board'] == board) & (input_df['row'] == pix[0]) & (input_df['col'] == pix[1])
+            )
+            masks[board] = mask
+
+        # Combine the masks using logical OR
+        combined_mask = pd.concat(masks, axis=1).any(axis=1)
+
+        # Apply the combined mask to the DataFrame
+        filtered = input_df[combined_mask].reset_index(drop=True)
+        return filtered
+
+    ## --------------------------------------
+    def find_maximum_event_combination(
+            self,
+            input_df: pd.DataFrame,
+            board_pixel_info: list,
+        ):
+        # Step 1: Filter the rows where board is 1, col is 6, and row is 15
+        selected_rows = input_df[(input_df['board'] == board_pixel_info[0]) & (input_df['row'] == board_pixel_info[1]) & (input_df['col'] == board_pixel_info[2])]
+
+        # Step 2: Get the unique "evt" values from the selected rows
+        unique_evts = selected_rows['evt'].unique()
+
+        # Step 3: Filter rows where board is 0 or 3 and "evt" is in unique_evts
+        filtered_rows = input_df[(input_df['board'].isin([0, 3])) & (input_df['evt'].isin(unique_evts))]
+
+        result_df = pd.concat([selected_rows, filtered_rows], ignore_index=True)
+        result_df = result_df.sort_values(by="evt")
+        result_df.reset_index(inplace=True, drop=True)
+
+        test_group = result_df.groupby(['board', 'row', 'col'])
+        count_df = test_group.size().reset_index(name='count')
+
+        row0 = count_df.loc[count_df[count_df['board'] == 0]['count'].idxmax()]
+        row3 = count_df.loc[count_df[count_df['board'] == 3]['count'].idxmax()]
+
+        print(f"Board 0, Row: {row0['row']}, Col: {row0['col']}, Count: {row0['count']}")
+        print(f"Board 3, Row: {row3['row']}, Col: {row3['col']}, Count: {row3['count']}")
+
+        del selected_rows, unique_evts, filtered_rows, test_group, count_df, row0, row3
+        return result_df
+
+    ## --------------------------------------
+    def iterative_timewalk_correction(
+        self,
+        input_df: pd.DataFrame,
+        iterative_cnt: int,
+        poly_order: int,
+    ):
+
+        corr_toas = []
+        coeffs = []
+
+        del_toa_b0 = (0.5*(input_df['toa_b1'] + input_df['toa_b3']) - input_df['toa_b0']).values
+        del_toa_b1 = (0.5*(input_df['toa_b0'] + input_df['toa_b3']) - input_df['toa_b1']).values
+        del_toa_b3 = (0.5*(input_df['toa_b0'] + input_df['toa_b1']) - input_df['toa_b3']).values
+
+        for i in range(iterative_cnt):
+            coeff_b0 = np.polyfit(input_df['tot_b0'].values, del_toa_b0, poly_order)
+            poly_func_b0 = np.poly1d(coeff_b0)
+
+            coeff_b1 = np.polyfit(input_df['tot_b1'].values, del_toa_b1, poly_order)
+            poly_func_b1 = np.poly1d(coeff_b1)
+
+            coeff_b3 = np.polyfit(input_df['tot_b3'].values, del_toa_b3, poly_order)
+            poly_func_b3 = np.poly1d(coeff_b3)
+
+            corr_b0 = input_df['toa_b0'].values + poly_func_b0(input_df['tot_b0'].values)
+            corr_b1 = input_df['toa_b1'].values + poly_func_b1(input_df['tot_b1'].values)
+            corr_b3 = input_df['toa_b3'].values + poly_func_b3(input_df['tot_b3'].values)
+
+            del_toa_b0 = (0.5*(corr_b1 + corr_b3) - corr_b0)
+            del_toa_b1 = (0.5*(corr_b0 + corr_b3) - corr_b1)
+            del_toa_b3 = (0.5*(corr_b0 + corr_b1) - corr_b3)
+
+            if i == iterative_cnt-1:
+
+                coeff_b0 = np.polyfit(input_df['tot_b0'].values, del_toa_b0, poly_order)
+                coeff_b1 = np.polyfit(input_df['tot_b1'].values, del_toa_b1, poly_order)
+                coeff_b3 = np.polyfit(input_df['tot_b3'].values, del_toa_b3, poly_order)
+
+                coeffs.append(coeff_b0)
+                coeffs.append(coeff_b1)
+                coeffs.append(coeff_b3)
+
+                corr_toas.append(corr_b0)
+                corr_toas.append(corr_b1)
+                corr_toas.append(corr_b3)
+
+        return coeffs, corr_toas
+
+    ## --------------------------------------
     def making_3d_heatmap_byPandas(
             self,
             input_df: pd.DataFrame,
@@ -260,7 +402,51 @@ class etroc2_analysis_helper():
             h[chip_names[idx]].fill(tmp_df['cal'].values, tmp_df['tot'].values, tmp_df['toa'].values)
 
         return h
+    ## --------------------------------------
+    def return_resolution_ps(self, sig_a, err_a, sig_b, err_b, sig_c, err_c):
+        res = np.sqrt(0.5)*(np.sqrt(sig_a**2 + sig_b**2 - sig_c**2))
+        var_res = (1/4)*(1/res**2)*(((sig_a**2)*(err_a**2))+((sig_b**2)*(err_b**2))+((sig_c**2)*(err_c**2)))
+        return res*1e3, np.sqrt(var_res)*1e3
 
+    ## --------------------------------------
+    def draw_hist_plot_pull(
+        self,
+        input_hist: hist.Hist,
+        fig_title: str,
+    ):
+        fig = plt.figure(figsize=(15, 10))
+        grid = fig.add_gridspec(2, 1, hspace=0, height_ratios=[3, 1])
+        main_ax = fig.add_subplot(grid[0])
+        subplot_ax = fig.add_subplot(grid[1], sharex=main_ax)
+        plt.setp(main_ax.get_xticklabels(), visible=False)
+
+        main_ax_artists, sublot_ax_arists = input_hist.plot_pull(
+            "gaus",
+            eb_ecolor="steelblue",
+            eb_mfc="steelblue",
+            eb_mec="steelblue",
+            eb_fmt="o",
+            eb_ms=6,
+            eb_capsize=1,
+            eb_capthick=2,
+            eb_alpha=0.8,
+            fp_c="hotpink",
+            fp_ls="-",
+            fp_lw=2,
+            fp_alpha=0.8,
+            bar_fc="royalblue",
+            pp_num=3,
+            pp_fc="royalblue",
+            pp_alpha=0.618,
+            pp_ec=None,
+            ub_alpha=0.2,
+            fit_fmt= r"{name} = {value:.4g} $\pm$ {error:.4g}",
+            ax_dict= {"main_ax":main_ax,"pull_ax":subplot_ax},
+        )
+        hep.cms.text(loc=0, ax=main_ax, text="Preliminary", fontsize=25)
+        main_ax.set_title(f'{fig_title}', loc="right", size=25)
+
+    ## --------------------------------------
     def make_pix_inclusive_plots(
             self,
             input_hist: hist.Hist,
@@ -313,6 +499,7 @@ class etroc2_analysis_helper():
         if(show): plt.show()
         plt.close()
 
+    ## --------------------------------------
     def event_display_withPandas(
             self,
             input_df: pd.DataFrame,
