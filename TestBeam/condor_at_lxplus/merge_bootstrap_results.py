fb1ba7a4e1381baaaafe02d14e494a056c2231ff
Quantumapple <jongapples@gmail.com>
2024-03-28 16:11:12 -0500

    use gaussian fit to get the final number

--- TestBeam/condor_at_lxplus/merge_bootstrap_results.py
+++ TestBeam/condor_at_lxplus/merge_bootstrap_results.py
@@ -5,6 +5,9 @@ from glob import glob
 import pandas as pd
 import re
 from tqdm import tqdm
+from lmfit.models import GaussianModel
+import hist
+import numpy as np
 
 parser = argparse.ArgumentParser(
             prog='PlaceHolder',
@@ -36,6 +39,9 @@ args = parser.parse_args()
 final_dict = defaultdict(list)
 files = natsorted(glob(args.dirname+'/*pkl'))
 
+mod = GaussianModel(nan_policy='omit')
+
+
 for ifile in tqdm(files):
 
     # Define the pattern to match "RxCx" part
@@ -48,11 +54,25 @@ for ifile in tqdm(files):
     columns = df.columns
 
     for idx in range(len(columns)):
+
+        h_temp = hist.Hist(hist.axis.Regular(100, 0, 100, name="time_resolution", label=r'Time Resolution [ps]'))
+        h_temp.fill(df[columns[idx]])
+        centers = h_temp.axes[0].centers
+
+        fit_constrain = (centers > df[columns[idx]].astype(int).mode()[0]-7) & (centers < df[columns[idx]].astype(int).mode()[0]+7)
+
+        pars = mod.guess(h_temp.values()[fit_constrain], x=centers[fit_constrain])
+        out = mod.fit(h_temp.values()[fit_constrain], pars, x=centers[fit_constrain], weights=1/np.sqrt(h_temp.values()[fit_constrain]))
+
         final_dict[f'row{columns[idx]}'].append(matches[idx][0])
         final_dict[f'col{columns[idx]}'].append(matches[idx][1])
+        if abs(out.params['sigma'].value) < 10:
+            final_dict[f'res{columns[idx]}'].append(out.params['center'].value)
+            final_dict[f'err{columns[idx]}'].append(abs(out.params['sigma'].value))
+        else:
+            final_dict[f'res{columns[idx]}'].append(np.mean(df[columns[idx]]))
+            final_dict[f'err{columns[idx]}'].append(np.std(df[columns[idx]]))
 
-        final_dict[f'res{columns[idx]}'].append(df[columns[idx]].mean())
-        final_dict[f'err{columns[idx]}'].append(df[columns[idx]].std())
 
 final_df = pd.DataFrame(final_dict)
 final_df.to_csv(args.outname+'.csv', index=False)
