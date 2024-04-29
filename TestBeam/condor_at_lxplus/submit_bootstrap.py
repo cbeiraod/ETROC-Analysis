3e5fcf0e8e984e134d2166ba76ae066c8b9eb29b
Quantumapple <jongapples@gmail.com>
2024-04-29 17:21:39 -0500

    match option name

--- TestBeam/condor_at_lxplus/submit_bootstrap.py
+++ TestBeam/condor_at_lxplus/submit_bootstrap.py
@@ -39,7 +39,6 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    '-n',
     '--minimum_nevt',
     metavar = 'NUM',
     type = int,
@@ -114,8 +113,8 @@ pwd
 # Load python environment from work node
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
 
-echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts"
-python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts
+echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts"
+python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts
     """.format(args.iteration, args.sampling, args.minimum_nevt, args.trigTOALower, args.trigTOAUpper)
 elif (args.autoTOTcuts) & (args.noTrig):
     bash_script = """#!/bin/bash
@@ -127,8 +126,8 @@ pwd
 # Load python environment from work node
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
 
-echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts --noTrig"
-python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts --noTrig
+echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts --noTrig"
+python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4} --autoTOTcuts --noTrig
     """.format(args.iteration, args.sampling, args.minimum_nevt, args.trigTOALower, args.trigTOAUpper)
 
 else:
@@ -141,8 +140,8 @@ pwd
 # Load python environment from work node
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
 
-echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4}"
-python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} -n {2} --trigTOALower {3} --trigTOAUpper {4}
+echo "python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4}"
+python bootstrap.py -f ${{1}}.pkl -i {0} -s {1} --minimum_nevt {2} --trigTOALower {3} --trigTOAUpper {4}
     """.format(args.iteration, args.sampling, args.minimum_nevt, args.trigTOALower, args.trigTOAUpper)
 
 print('\n========= Run option =========')
