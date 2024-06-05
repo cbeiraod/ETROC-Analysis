7562105e9827eaf10bc0e398bc9298a0ba7918b1
Quantumapple <jongapples@gmail.com>
2024-06-05 17:39:41 +0200

    finally fix the legend

--- TestBeam/beamtest_analysis_helper.py
+++ TestBeam/beamtest_analysis_helper.py
@@ -2323,6 +2323,27 @@ def plot_TWC(
     coeff_b2 = np.polyfit(input_df[f'tot_b{board_list[2]}'].values, del_toa_b2, poly_order)
     poly_func_b2 = np.poly1d(coeff_b2)
 
+    def make_legend(coeff, poly_order):
+        legend_str = ""
+        for i in range(poly_order + 1):
+            if round(coeff[i], 2) == 0:
+                # Use scientific notation
+                coeff_str = f"{coeff[i]:.2e}"
+            else:
+                # Use fixed-point notation
+                coeff_str = f"{coeff[i]:.2f}"
+
+            # Add x
+            coeff_str = rf"{coeff_str}$x^{poly_order-i}$"
+
+            # Add sign
+            if coeff[i] > 0:
+                coeff_str = f"+{coeff_str}"
+                legend_str += coeff_str
+            else:
+                legend_str += coeff_str
+        return legend_str
+
     if print_func:
         print(poly_func_b0)
         print(poly_func_b1)
@@ -2331,22 +2352,26 @@ def plot_TWC(
     fig, axes = plt.subplots(1, 3, figsize=(38, 10))
     hep.hist2dplot(h_twc1, ax=axes[0], norm=colors.LogNorm())
     hep.cms.text(loc=0, ax=axes[0], text="Phase-2 Preliminary", fontsize=20)
-    axes[0].plot(b1_xrange, poly_func_b0(b1_xrange), 'r-', lw=3, label='linear fit')
+    axes[0].plot(b1_xrange, poly_func_b0(b1_xrange), 'r-', lw=3, label=make_legend(coeff_b0, poly_order=poly_order))
     axes[0].set_xlabel('TOT1 [ps]', fontsize=18)
     axes[0].set_ylabel('0.5*(TOA2+TOA3)-TOA1 [ps]', fontsize=18)
     axes[0].set_title(plot_title, fontsize=18, loc='right')
     hep.hist2dplot(h_twc2, ax=axes[1], norm=colors.LogNorm())
     hep.cms.text(loc=0, ax=axes[1], text="Phase-2 Preliminary", fontsize=20)
-    axes[1].plot(b2_xrange, poly_func_b1(b2_xrange), 'r-', lw=3, label='linear fit')
+    axes[1].plot(b2_xrange, poly_func_b1(b2_xrange), 'r-', lw=3, label=make_legend(coeff_b1, poly_order=poly_order))
     axes[1].set_xlabel('TOT2 [ps]', fontsize=18)
     axes[1].set_ylabel('0.5*(TOA1+TOA3)-TOA2 [ps]', fontsize=18)
     axes[1].set_title(plot_title, fontsize=18, loc='right')
     hep.hist2dplot(h_twc3, ax=axes[2], norm=colors.LogNorm())
     hep.cms.text(loc=0, ax=axes[2], text="Phase-2 Preliminary", fontsize=20)
-    axes[2].plot(b3_xrange, poly_func_b2(b3_xrange), 'r-', lw=3, label='linear fit')
+    axes[2].plot(b3_xrange, poly_func_b2(b3_xrange), 'r-', lw=3, label=make_legend(coeff_b2, poly_order=poly_order))
     axes[2].set_xlabel('TOT3 [ps]', fontsize=18)
     axes[2].set_ylabel('0.5*(TOA1+TOA2)-TOA3 [ps]', fontsize=18)
     axes[2].set_title(plot_title, fontsize=18, loc='right')
+
+    axes[0].legend(loc='best')
+    axes[1].legend(loc='best')
+    axes[2].legend(loc='best')
 
     if distance is not None:
         axes[0].fill_between(b1_xrange, y1=poly_func_b0(b1_xrange)-boundary_cut*np.std(distance[0]), y2=poly_func_b0(b1_xrange)+boundary_cut*np.std(distance[0]),
@@ -2356,9 +2381,9 @@ def plot_TWC(
         axes[2].fill_between(b3_xrange, y1=poly_func_b2(b3_xrange)-boundary_cut*np.std(distance[2]), y2=poly_func_b2(b3_xrange)+boundary_cut*np.std(distance[2]),
                 facecolor='red', alpha=0.35, label=fr'{boundary_cut}$\sigma$ boundary')
 
-        axes[0].legend(loc='best')
-        axes[1].legend(loc='best')
-        axes[2].legend(loc='best')
+        axes[0].legend(['linear fit'], loc='best')
+        axes[1].legend(['linear fit'], loc='best')
+        axes[2].legend(['linear fit'], loc='best')
 
     plt.tight_layout()
 
