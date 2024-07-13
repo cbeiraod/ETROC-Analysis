4d82978c484498bbfed6a837e15b88cf12484664
Jongho Lee <jongho.lee@cern.ch>
2024-07-13 15:09:59 -0500

    change to use python 3.11

--- TestBeam/condor_at_lpc/load_python39.sh
+++ TestBeam/condor_at_lpc/load_python39.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
+#source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
